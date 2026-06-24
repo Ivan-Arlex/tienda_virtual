191c24c4c2c0b98a5dcfcf98f15fd30449c6c631
Ivan-Arlex <ivanriascos063@gmail.com>
2026-06-23 23:16:17 -0500

    Inicio de Proyecto

---
+++ src/test/java/com/unipacifico_ingenieri_sistemas/tienda_virtual/TiendaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipacifico_ingenieri_sistemas.tienda_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
